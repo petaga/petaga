3aa622122a18bea13c9fef1bb787443e0a32243d
Sam Winslow <sammywinslow@gmail.com>
2014-07-08 20:46:17 -0700

    Fixed 101 scaffolding and build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd templates
 mustache index.mustache > ../index.html
-mustache 101.mustache > ../pet-insurance-101.html
+mustache pet-insurance-101.mustache > ../pet-insurance-101.html
 echo "Build completed at $(date)"
