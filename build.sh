e9468f44c744a67c6d0c84e5d906aa00ee220c45
Andy Plaisted <andrew.plaisted@gmail.com>
2014-07-18 18:21:11 -0700

    Updated about name + fised footer

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ cd templates
 mustache index.mustache > ../index.html
 mustache pet-insurance-101.mustache > ../pet_insurance_101.html
 mustache pet-savings.mustache > ../savings_101.html
-mustache about.mustache >../about.html
+mustache about.mustache >../about_us.html
 mustache team.mustache > ../team.html
 echo "Build completed at $(date)"
