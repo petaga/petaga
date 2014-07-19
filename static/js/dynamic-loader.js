00b927725ae7fca640066b3bfb0b6e8ccb46a788
Sam Winslow <sammywinslow@gmail.com>
2014-07-19 12:44:46 -0700

    Dynamic loader things

--- static/js/dynamic-loader.js
+++ static/js/dynamic-loader.js
@@ -1,31 +1,18 @@
-var data = {data: [
-  	{ "firstname": "Ray",
-  		"lastname": "Villalobos",
-  		"category": "Front End",
-  		"title": "Bootstrap & CSS Preprocessors",
-  		"image": "http://barcampdeland.org/images/speaker_rayvillalobos.jpg",
-  		"link": "http://iviewsource.com",
-  	},
-  	{ "firstname": "Sam",
-  		"lastname": "Winslow",
-  		"category": "Front End",
-  		"title": "Swag & Good Grades",
-  		"image": "http://placehold.it/400",
-  		"link": "http://samwinslow.me/",
-  	},
-  	{ "firstname": "Andy",
-  		"lastname": "Plaisted",
-  		"category": "Front End",
-  		"title": "Awesome things @ Morgan Stanley",
-  		"image": "http://placehold.it/400",
-  		"link": "http://samwinslow.me/",
-  	}
-]};
 $(document).ready(function(){
   $('.view').each(function(i, obj){
     dataTemplate = $(obj).attr("data-template");
+    requestURL = "http://localhost/petaga-backend/request.php?template="+dataTemplate;
     parseIn = $(obj).html().replace(/\[\[/g,"{{").replace(/\]\]/g,"}}");
-    var compiled = Mustache.to_html(parseIn, data);
-    $(obj).html(compiled);
+    $.ajax({
+      url:requestURL,
+      dataType: 'jsonp',
+      success:function(json){
+        var compiled = Mustache.to_html(parseIn, json);
+        $(obj).html(compiled);
+      },
+      error:function(){
+        console.log("JSON error");
+      }
+    });
   });
 });
